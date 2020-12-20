348e2218addc22bc548447960d8df0b2b1418ea1
Ciaran Archer <carcher@zendesk.com>
2020-12-20 19:31:00 +0000

    Day 2 part1 done.

--- day2/part1.rb
+++ day2/part1.rb
@@ -15,4 +15,31 @@ class PwdPolicy
     @max = parts[1].to_i
     @char = parts[2]
   end
+
+  ##
+  # Check password against policy
+  def check(pwd)
+    chars = pwd.split('')
+    matches = chars.filter { |char| char == @char }
+    len = matches.length
+
+    return true if len >= @min && len <= @max
+
+    false
+  end
 end
+
+input = File.read 'input.txt'
+lines = input.split("\n")
+
+count_valid = 0
+
+lines.each do |l|
+  parts = l.split(':')
+  policy = parts.first
+  pwd = parts.last
+
+  count_valid += 1 if PwdPolicy.new(policy).check(pwd)
+end
+
+puts "Of #{lines.length} passwords, #{count_valid} are valid."
--- day2/part1_spec.rb
+++ day2/part1_spec.rb
@@ -3,11 +3,13 @@
 require_relative 'part1'
 
 RSpec.describe PwdPolicy, '#init' do
-  it 'fails with an invalid policy' do
-    expect { PwdPolicy.new('12-12-12') }.to raise_error('invalid policy code')
+  context 'invalid policy' do
+    it 'fails with an invalid policy' do
+      expect { PwdPolicy.new('12-12-12') }.to raise_error('invalid policy code')
+    end
   end
 
-  context 'sane policy' do
+  context 'valid policy' do
     let(:policy) { PwdPolicy.new('2-5 c') }
 
     it 'has a min number of chars' do
@@ -16,6 +18,25 @@ RSpec.describe PwdPolicy, '#init' do
 
     it 'has a max number of chars' do
       expect(policy.max).to be 5
+    end
+
+    describe '#check' do
+      let(:policy) { PwdPolicy.new('2-5 c') }
+
+      describe 'valid pwd' do
+        it 'accepts pwds matching policy' do
+          expect(policy.check('ycyccik')).to be true
+          expect(policy.check('yccyccik')).to be true
+          expect(policy.check('yccyccikc')).to be true
+        end
+      end
+
+      describe 'invalid pwd' do
+        it 'rejects pwds not matching policy' do
+          expect(policy.check('yyikkwer')).to be false
+          expect(policy.check('ycickccrcccc')).to be false
+        end
+      end
     end
   end
 end
